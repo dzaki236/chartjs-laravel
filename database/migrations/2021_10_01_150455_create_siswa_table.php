55f958f9833432d7ce264eb2b48c8e1efb7d76bd
dzaki236 <dzakiahnafz@gmail.com>
2021-10-01 22:08:51 +0700

    first commit

---
+++ database/migrations/2021_10_01_150455_create_siswa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_lengkap',100);
+            $table->enum('jenis_kelamin',['l','p','nothing'])->default('nothing');
+            $table->text('alamat');
+            $table->string('kelas');
+            $table->date('tanggal_lahir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+}
